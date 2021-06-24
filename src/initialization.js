6571803c260a1341d38dae9da9c834600abffb81
Jonathan Ng <jonathan.ng@stonybrook.edu>
2021-06-24 04:09:37 -0400

    sidebar now loads content and tab switches properly

--- src/initialization.js
+++ src/initialization.js
@@ -1,40 +1,85 @@
 function initializeDOM(){
-    const sidebar = document.getElementById("sidebar");
+    const container = document.getElementById("container");
+    const sidebar = document.createElement("div");
+    sidebar.setAttribute("id", "sidebar");
+    container.appendChild(sidebar);
+
+    const content = document.createElement("div");
+    content.setAttribute("id", "content");
+    content.classList.add("content");
+
     let inbox = document.createElement("button");
     inbox.setAttribute("class", "button-active");
+
+    inbox.setAttribute("id", "inbox");
     inbox.textContent = "Inbox";
-    inbox.addEventListener("click", () => {
-        const buttons = document.querySelectorAll(".button-active");
-        buttons.forEach(button => {
-            button.classList.toggle("button-active");
-        });
-        inbox.classList.toggle("button-active");
-
+    inbox.addEventListener("click", e => {
+        clickTab(e);
+        loadInboxContent();
     });
-
     sidebar.appendChild(inbox);
 
     let today = document.createElement("button");
     today.textContent = "Today";
-    today.addEventListener("click", () => {
-        const buttons = document.querySelectorAll(".button-active");
-        buttons.forEach(button => {
-            button.classList.toggle("button-active");
-        });
-        today.classList.toggle("button-active");
+    today.setAttribute("id", "today");
+    today.addEventListener("click", e => {
+        clickTab(e);
+        loadTodayContent();
     });
     sidebar.appendChild(today);
 
     let week = document.createElement("button");
     week.textContent = "This Week";
-    week.addEventListener("click", () => {
-        const buttons = document.querySelectorAll(".button-active");
-        buttons.forEach(button => {
-            button.classList.toggle("button-active");
-        });
-        week.classList.toggle("button-active");
+    week.setAttribute("id", "week")
+    week.addEventListener("click", e => {
+        clickTab(e);
+        loadWeekContent();
+
     });
     sidebar.appendChild(week);
+
+    container.appendChild(content);
+    loadInboxContent();
+
+
+}
+
+function clickTab(e){
+    const buttons = document.querySelectorAll(".button-active");
+    buttons.forEach(button => {
+        button.classList.toggle("button-active");
+    });
+    e.target.classList.toggle("button-active");
+}
+
+function replaceContent(){
+    const container = document.getElementById("container");
+    const originalContent = document.getElementById("content");
+    const content = document.createElement("div");
+    content.classList.add("content");
+    content.setAttribute("id", "content");
+
+    container.replaceChild(content, originalContent);
+    return content;
+
+}
+
+function loadInboxContent(){
+    const inboxContent = replaceContent();
+    inboxContent.textContent = "inbox";
+
+
+}
+
+function loadTodayContent(){
+    const todayConent = replaceContent();
+    todayConent.textContent = "today";
+
+}
+
+function loadWeekContent(){
+    const weekContent = replaceContent();
+    weekContent.textContent = "week";
 
 }
 
